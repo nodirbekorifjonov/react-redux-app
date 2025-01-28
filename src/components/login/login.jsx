665f4d2bd48ac0cd3995bcc804d180220580504f
Nodirbek Orifjonov <nodirbekorifjonov@mail.ru>
2025-01-28 21:03:35 +0500

    #9. Login & Register pages & UI

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,6 +1,32 @@
+import { logo } from "../constants/constants";
+import { Input } from "../../ui";
+import { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="d-flex align-items-center py-4 text-center mt-5">
+      <main className="form-signin w-25 m-auto">
+        <form>
+          {logo}
+          <h1 className="h3 mb-3 fw-normal">Please login</h1>
+
+          <Input label={"Email address"} state={email} setState={setEmail} />
+          <Input
+            label={"Password"}
+            type="password"
+            state={password}
+            setState={setPassword}
+          />
+          <button className="btn btn-primary w-100 py-2" type="submit">
+            Login
+          </button>
+          <p className="mt-5 mb-3 text-body-secondary">© 2017–2024</p>
+        </form>
+      </main>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,5 +1,35 @@
+import { logo } from "../constants/constants";
+import { Input } from "../../ui";
+import { useState } from "react";
+
 const Register = () => {
-  return <div>Register</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="d-flex align-items-center py-4 text-center mt-5">
+      <main className="form-signin w-25 m-auto">
+        <form>
+          {logo}
+          <h1 className="h3 mb-3 fw-normal">Please register</h1>
+
+          <Input label={"Username"} state={name} setState={setName} />
+          <Input label={"Email address"} state={email} setState={setEmail} />
+          <Input
+            label={"Password"}
+            type="password"
+            state={password}
+            setState={setPassword}
+          />
+          <button className="btn btn-primary w-100 py-2" type="submit">
+            Register
+          </button>
+          <p className="mt-5 mb-3 text-body-secondary">© 2017–2024</p>
+        </form>
+      </main>
+    </div>
+  );
 };
 
 export default Register;
