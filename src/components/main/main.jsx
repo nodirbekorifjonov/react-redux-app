c9e875eece0c2e58a71e3574dcec503f309906d7
Nodirbek Orifjonov <nodirbekorifjonov@mail.ru>
2025-01-29 20:44:08 +0500

    #21. Loader

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,9 +1,11 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Loader } from "../../ui";
 
 const Main = () => {
-  const { articles } = useSelector((state) => state.article);
+  const { articles, isLoading } = useSelector((state) => state.article);
   return (
     <div className="container">
+      {isLoading && <Loader />}
       <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
         {articles.map((item) => (
           <div key={item.id} className="col">
@@ -22,33 +24,34 @@ const Main = () => {
                 <rect width="100%" height="100%" fill="#55595c"></rect>
               </svg>
               <div className="card-body">
-                <p className="card-text fw-bold">{item.title}</p>
+                <p className="card-text fw-bold m-0">{item.title}</p>
                 <p className="card-text">{item.description}</p>
-                <div className="d-flex justify-content-between align-items-center">
-                  <div className="btn-group">
-                    <button
-                      type="button"
-                      className="btn btn-sm btn-outline-success"
-                    >
-                      View
-                    </button>
-                    <button
-                      type="button"
-                      className="btn btn-sm btn-outline-secondary"
-                    >
-                      Edit
-                    </button>
-                    <button
-                      type="button"
-                      className="btn btn-sm btn-outline-danger"
-                    >
-                      Delete
-                    </button>
-                  </div>
-                  <small className="text-body-secondary text-capitalize fw-bold">
-                    {item.author.username}
-                  </small>
+              </div>
+
+              <div className="card-footer d-flex justify-content-between align-items-center">
+                <div className="btn-group">
+                  <button
+                    type="button"
+                    className="btn btn-sm btn-outline-success"
+                  >
+                    View
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-sm btn-outline-secondary"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-sm btn-outline-danger"
+                  >
+                    Delete
+                  </button>
                 </div>
+                <small className="text-body-secondary text-capitalize fw-bold">
+                  {item.author.username}
+                </small>
               </div>
             </div>
           </div>
